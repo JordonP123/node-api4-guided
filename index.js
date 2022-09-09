aaa428e8916f06f7b436e3857002bedbc4b87ea3
JordonP123 <jordonaren@gmail.com>
2022-09-09 05:06:08 -0400

    yea

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./api/server.js");
 
-const port = 9000;
+const port = process.env.PORT || 9000;
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
